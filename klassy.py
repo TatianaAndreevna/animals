5468e0817d5c6ca005d142c72d4ad3b0cd1764f4
TatianaAndreevna <Tanjaverkhvceva@rambler.ru>
2018-11-28 22:56:50 +0300

    Исправления

--- klassy.py
+++ klassy.py
@@ -5,73 +5,90 @@ class Animals:
     gives_product = 0
     hungry = True
 
-
     def __init__(self, name, weight):
         self.name = name
         self.weight = weight
 
-
     def collect_product(self, volume_product):
-        self.volume_product -= gives_product
-
+        self.gives_product -= volume_product
 
     def food(self, feed):
-        sele.hungry = Folse
-        sele.weight += feed
+        self.hungry = False
+        self.weight += feed
 
 
-class Geese(Animals):
-   speech = 'Га-га'
-   gives_product = 1    #яйцо
+class Goose(Animals):
+    speech = 'Га-га'
+    gives_product = 1  # яйцо
 
 
-class Cows(Animals):
+class Cow(Animals):
     speech = 'Муу'
-    gives_product = 25    #л молока
+    gives_product = 25  # л молока
 
 
-class Sheeps(Animals):
+class Sheep(Animals):
     speech = 'Бее'
-    gives_product = 14   #кг шерсти
+    gives_product = 14  # кг шерсти
 
 
-class Chickens(Animals):
+class Chicken(Animals):
     speech = 'Ко-ко-ко'
-    gives_product = 1  #яйцо
+    gives_product = 1  # яйцо
 
 
-class Goats(Animals):
+class Goat(Animals):
     speech = 'Мее'
-    gives_product = 3  #л молока
+    gives_product = 3  # л молока
 
 
-class Ducks(Animals):
+class Duck(Animals):
     speech = 'Кря-кря'
-    gives_product = 1  #кг мяса
+    gives_product = 1  # кг мяса
 
 
-goose_1 = Geese('Серый', 3)
-goose_2 = Geese('Белый', 2)
-cow = Cows('Манька', 500)
-sheep_1 = Sheeps('Барашек', 80)
-sheep_2 = Sheeps('Кудрявый', 120)
-chicken_1 = Chickens('Ко-ко', 1)
-chicken_2 = Chickens('Кукареку', 2)
-goat_1 = Goats('Рога', 60)
-goat_2 = Goats('Копыта', 70)
-duck = Ducks('Кряква', 1)
+goose_1 = Goose('Серый', 3)
+goose_2 = Goose('Белый', 2)
+cow = Cow('Манька', 500)
+sheep_1 = Sheep('Барашек', 80)
+sheep_2 = Sheep('Кудрявый', 120)
+chicken_1 = Chicken('Ко-ко', 1)
+chicken_2 = Chicken('Кукареку', 2)
+goat_1 = Goat('Рога', 60)
+goat_2 = Goat('Копыта', 70)
+duck = Duck('Кряква', 1)
 
+goose_1.collect_product(3)
+goose_2.collect_product(3)
+cow.collect_product(25)
+sheep_1.collect_product(14)
+sheep_2.collect_product(14)
+chicken_1.collect_product(1)
+chicken_2.collect_product(1)
+goat_1.collect_product(3)
+goat_2.collect_product(3)
+duck.collect_product(1)
+
+goose_1.food(1)
+goose_2.food(1)
+cow.food(10)
+sheep_1.food(5)
+sheep_2.food(5)
+chicken_1.food(1)
+chicken_2.food(1)
+goat_1.food(3)
+goat_2.food(3)
+duck.food(1)
 
 animal_list = [goose_1, goose_2, cow, sheep_1, sheep_2, chicken_1, chicken_2, goat_1, goat_2, duck]
-
 
 total_weight = 0
 max_weight = 0
 heaviest_animal = str()
 for animal in animal_list:
-  total_weight += animal.weight
-  if animal.weight > max_weight:
-    max_weight = animal.weight
-    heaviest_animal = animal.name
+    total_weight += animal.weight
+    if animal.weight > max_weight:
+        max_weight = animal.weight
+        heaviest_animal = animal.name
 print('Суммарный вес всех животных {} кг'.format(total_weight))
 print('Самое тяжелое животное зовут {}'.format(heaviest_animal))
